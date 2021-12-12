4b27e6eac73dc4ea5b470aaa34490c0580f71d91
EC2 Default User <ec2-user@ip-172-31-24-195.us-east-2.compute.internal>
2021-12-12 02:49:57 +0000

    make fail test on purpose

--- squareup_api/tests/test_4_fun.py
+++ squareup_api/tests/test_4_fun.py
@@ -3,9 +3,9 @@ import unittest
 class Test4Fun(unittest.TestCase):
     
     def test_4_fun(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1+1,2)
         
     def test_4_fun_2(self): # fail on purpose
-        self.assertEqual(1,1)
+        self.assertEqual(1+1,3)
         
     
